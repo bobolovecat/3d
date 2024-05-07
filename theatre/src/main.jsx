a0e0f3f6f50fb16b4cb5ea8628b2331e63039053
bobolovecat <youngmaple@qq.com>
2024-05-07 12:57:46 +0800

    feat(main): init

---
+++ theatre/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import studio from '@theatre/studio'
+import extension from '@theatre/r3f/dist/extension'
+
+studio.extend(extension)
+studio.initialize()
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Suspense fallback={null}>
+            <App />
+        </Suspense>
+    </React.StrictMode>
+);
