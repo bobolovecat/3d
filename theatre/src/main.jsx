80cbefc7008166407e6ac719bc224eaee86cf60e
bobolovecat <youngmaple@qq.com>
2024-05-08 14:32:50 +0800

    feat(theatre): 3d模型加载

--- theatre/src/main.jsx
+++ theatre/src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import App from './App.jsx'
+import App from './App2.jsx'
+import './App.css'
 import studio from '@theatre/studio'
 import extension from '@theatre/r3f/dist/extension'
 
-studio.extend(extension)
-studio.initialize()
+// studio.extend(extension)
+// studio.initialize()
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Suspense fallback={null}>
-            <App />
-        </Suspense>
+        <App />
     </React.StrictMode>
 );
