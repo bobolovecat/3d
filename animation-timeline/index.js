e735653a6bc3ab992b43afabd6daa6f538621b75
bobolovecat <youngmaple@qq.com>
2024-11-01 18:05:45 +0800

    feat(animation-timeline): init

---
+++ animation-timeline/index.js
@@ -0,0 +1,6 @@
+import { trackProgress } from "https://esm.sh/@bramus/sda-utilities";
+
+const model = document.querySelector("model-viewer");
+trackProgress(model.getAnimations()[0], (progress) => {
+	model.orientation = `0deg 0deg ${progress * -360}deg`;
+});
